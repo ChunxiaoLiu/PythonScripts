422140419c6773d30545f09554a450bd956ecde7
Chun-Xiao Liu <chunxiaoliu62@gmail.com>
2017-10-28 23:17:04 -0400

    To save and load data.

---
+++ data_process.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+fileHandle = open('name.txt','w'); #write data of float type into a txt. file.
+for i in range(Number):
+    fileHandle.write( str(theArray[i]) + ',' );
+fileHandle.write('\n');
+fileHandle.close();
+
+
+# In[ ]:
+
+
+
